379a7beeefde056585ab158ed557432b67a8b9b4
DESKTOP-JSKLQUE\Vinisha D'Souza <vinisha.n.dsouza@gmail.com>
2018-02-21 12:56:38 -0500

    added rates.js

---
+++ getRates.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',function(){
+			input.addEventListener('keyup',function(){
+				//API Call based on the selected currencies
+				fetch(`https://api.fixer.io/latest?base=${first.value}&symbols=${second.value}`)
+				.then(res => res.json())// convert to json object
+				.then((out) => { // output object
+					input.value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+					let rate = out.rates[Object.keys(out.rates)[0]];
+					//Rate does not exist if Base and Symbol is the same
+					if (rate == false) {
+						converted.value = input.value;
+					}else{
+						converted.value = (rate * input.value).toFixed(2);
+					}
+				})
+				.catch(err => { throw err });
+			})
+			
+		});
+	
+	// Definie our currency-converter custom element
+
+
+
