ae9e119789abeaaf5185131b2d330e1f41f82af8
Vinisha D'Souza <VinishaDsouza@users.noreply.github.com>
2019-03-15 13:48:54 -0400

    Update getRates.js

--- scripts/getRates.js
+++ scripts/getRates.js
@@ -5,7 +5,7 @@ function onSelect(){
 				.then(res => res.json())// convert to json object
 				.then((out) => { 
 					input.value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-					let rate = out.rates[Object.keys(out.rates)];
+					let rate = out.rates[0];
 					//Rate does not exist if Base and Symbol is the same
 					if (isNaN(rate)) {
 						converted.value = input.value;
