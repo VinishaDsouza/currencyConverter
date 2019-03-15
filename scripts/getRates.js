f07013267edbc6d25023cda77013e60736a4f8e8
Vinisha D'Souza <VinishaDsouza@users.noreply.github.com>
2019-03-15 12:05:47 -0400

    c

--- scripts/getRates.js
+++ scripts/getRates.js
@@ -1,7 +1,7 @@
 
 function onSelect(){
 				//API Call based on the selected currencies
-				fetch(`http://data.fixer.io/api/latest?access_key=e6e9e12159406c5476fe0ffbd8082f40&format=1`)
+				fetch(`http://data.fixer.io/api/latest?access_key=e6e9e12159406c5476fe0ffbd8082f40`)
 				.then(res => res.json())// convert to json object
 				.then((out) => { 
 					input.value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
