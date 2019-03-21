188ca1636b90d7b4defd441c59e0035768aba316
Vinisha D'Souza <VinishaDsouza@users.noreply.github.com>
2019-03-21 13:46:18 -0400

    the previous Api was not secure, needed to pay $$$, changed API and result

--- scripts/getRates.js
+++ scripts/getRates.js
@@ -1,11 +1,18 @@
 
+
 function onSelect(){
 				//API Call based on the selected currencies
-				fetch(`https://api.exchangeratesapi.io/latest`)
+				var a = document.getElementById("first").value;
+				var b = document.getElementById("second").value;
+
+				var uri= "https://api.exchangeratesapi.io/latest?base="+a;
+				fetch(uri)
 				.then(res => res.json())// convert to json object
 				.then((out) => { 
 					input.value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-					let rate = out.rates[0];
+					//console.log(input.value);
+					let rate = out.rates[b];
+					console.log(rate);
 					//Rate does not exist if Base and Symbol is the same
 					if (isNaN(rate)) {
 						converted.value = input.value;
